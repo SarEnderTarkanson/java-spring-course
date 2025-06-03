0c0c2d0673afa0252d5361f25b84496e20558bc3
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-03 08:10:08 +0200

    literal

--- core-java/Hello.java
+++ core-java/Hello.java
@@ -1,14 +1,14 @@
 class Hello {
     public static void main(String a[]) {
-        
-        byte b = 127;
-        short sh = 558;
-        long l = 5854l;
-        float f = 6.5f;
-        double d = 5.8;
-        char c = 'k';
-        boolean bool = true;
-        System.out.println();
-        
+
+        int num1 = 0b101;
+        System.out.println(num1);
+
+        char abc = 'a';
+        System.out.println(abc);
+
+        abc = abc++;
+        System.out.println(abc);
+
     }
 }
