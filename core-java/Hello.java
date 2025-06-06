89a01e61eab299e82eb9688c05f58f8da9fb6015
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-06 08:13:01 +0200

    logical operators

--- core-java/Hello.java
+++ core-java/Hello.java
@@ -1,12 +1,16 @@
 class Hello {
     public static void main(String args[]) {
 
-        double x = 9.8;
-        double y = 5.7;
+        int x = 7;
+        int y = 5;
+        int a = 5;
+        int b = 9;
 
-        boolean result = x != y;
+        boolean result = a >b;
 
-        System.out.println(result);
+
+
+        System.out.println(!result);
 
     }
 }
