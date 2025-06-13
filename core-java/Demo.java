efd863cc7b31a4af42a94c62e12e8a2f89418fb5
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-13 21:30:59 +0200

    if else if

---
+++ core-java/Demo.java
@@ -0,0 +1,17 @@
+class Demo {
+    public static void main(String args[]) {
+
+        int x = 8;
+        int y = 7;
+        int z = 9;
+
+        if (x > y && x > z) {
+            System.out.println(x);
+            // System.out.println("Thank you");
+        } else if (y > z && y > z)
+            System.out.println(y);
+        else
+            System.out.println(z);
+
+    }
+}
