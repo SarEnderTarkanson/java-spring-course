52901f8f1ffa961b2b64364a4fabcc2086091c9c
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-13 21:58:56 +0200

    switch statement

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,17 +1,33 @@
 class Demo {
     public static void main(String args[]) {
 
-        int n = 4;
-        int result = 0;
+        int n = 0;
 
-        // if (n % 2 == 0)
-        // result = 10;
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Enter a number between 1 and 7 (1 and 7 included)");
 
-        // else
-        // result = 20;
-
-        result = n % 2 == 0 ? 10 : 20;
-
-        System.out.println(result);
+        }
     }
 }
