e950b7b4e511a68d94bcc756e7f8444d5142e31a
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-16 17:35:11 +0200

    while loop

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,33 +1,16 @@
 class Demo {
     public static void main(String args[]) {
+        int i = 1;
+        while (i <= 5) {
 
-        int n = 0;
-
-        switch (n) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Enter a number between 1 and 7 (1 and 7 included)");
-
+            System.out.println(String.format("Week # %d", i));;
+            int j = 1;
+            while (j <= 7) {
+                System.out.println(String.format("The day # %d", j));
+                j++;
+            }
+            i++;
         }
+        System.out.println("Exit the entire loop.");
     }
 }
