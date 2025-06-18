1b99cd3bc62e3a20159c8021c00d9847f5e74979
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-18 17:57:28 +0200

    classes & objects

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,13 +1,25 @@
+class Calculator {
+
+    public int add(int n1, int n2) {
+
+        int r = n1 + n2;
+
+        return r;
+    }
+}
+
 class Demo {
     public static void main(String args[]) {
 
-        for (int i = 1; i <= 5; i++) {
+        int num1 = 4;
+        int num2 = 5;
 
-            System.out.println(String.format("Day # %d", i));
+        Calculator calc = new Calculator();
 
-            for (int j = 9; j <= 16; j++) {
-                System.out.println(String.format("Hr # %d", j));
-            }
-        }
+        int result = calc.add(num1, num2);
+        // int result = num1 + num2;
+
+        System.out.println(result);
+
     }
 }
