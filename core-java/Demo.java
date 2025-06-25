bf2c124c300e797e6d8d78e619dc5511fff296f0
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-25 23:49:10 +0200

    methods

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,25 +1,25 @@
-class Calculator {
+class Computer {
+    public void playMusic() {
 
-    public int add(int n1, int n2) {
+        System.out.println("Playing music...");
+    }
 
-        int r = n1 + n2;
-
-        return r;
+    public String getMeAPen(int cost) {
+        if (cost >= 10) {
+            return "Here is your pen";
+        }
+        return "The amount is too low for a pen";
     }
 }
 
-class Demo {
+public class Demo {
     public static void main(String args[]) {
 
-        int num1 = 4;
-        int num2 = 5;
+        Computer obj = new Computer();
 
-        Calculator calc = new Calculator();
-
-        int result = calc.add(num1, num2);
-        // int result = num1 + num2;
-
-        System.out.println(result);
+        obj.playMusic();
+        String str = obj.getMeAPen(10);
+        System.out.println(str);
 
     }
 }
