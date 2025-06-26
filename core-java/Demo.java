c59a38f9056c2f5c836b28a0326a0e9c9ba694a1
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-26 12:44:07 +0200

    stack and heap

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,14 +1,10 @@
 class Calculator {
 
-    public int add(int n1, int n2, int n3) {
-        return n1 + n2 + n3;
-    }
+    int num = 5;
 
     public int add(int n1, int n2) {
-        return n1 + n2;
-    }
 
-    public double add(double n1, int n2) {
+        // System.out.println(num);
         return n1 + n2;
     }
 }
@@ -16,10 +12,15 @@ class Calculator {
 public class Demo {
     public static void main(String args[]) {
 
+        int data = 10;
         Calculator obj = new Calculator();
+        Calculator obj2 = new Calculator();
 
-        int r1 = obj.add(3, 4);
-
-        System.out.println(r1);
+        obj.num = 10;
+        // int r1 = obj.add(3, 4);
+        // System.out.println(data);
+        // System.out.println(r1);
+        System.out.println(obj2.num);
+        System.out.println(obj.num);
     }
 }
