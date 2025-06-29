113ffa84eb5ca1e804965827769a736eddc1cfa7
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-29 23:48:32 +0200

    array of objects

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,26 +1,51 @@
+class Student {
+    int rollNo;
+    String name;
+    int marks;
+}
+
 public class Demo {
     public static void main(String args[]) {
 
-        //int nums[][][] = new int[3][4][5];
-        int nums[][] = new int[3][];
+        Student s1 = new Student();
 
-        nums[0] = new int[3];
-        nums[1] = new int[4];
-        nums[2] = new int[2];
+        s1.rollNo = 1;
+        s1.name = "Alpy";
+        s1.marks = 88;
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums[i].length; j++) {
-                nums[i][j] = (int) (Math.random() * 10);
-            }
+        Student s2 = new Student();
 
+        s2.rollNo = 2;
+        s2.name = "Beyza";
+        s2.marks = 68;
+
+        Student s3 = new Student();
+
+        s3.rollNo = 3;
+        s3.name = "Endo";
+        s3.marks = 97;
+
+        Student students[] = new Student[3];
+
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i].name);
         }
 
-        for (int n[] : nums) {
-            for (int m : n) {
-                System.out.print(m + " ");
-            }
-            System.out.println();
-        }
+        // int nums[] = new int[6];
+
+        // nums[0] = 4;
+        // nums[1] = 8;
+        // nums[2] = 3;
+        // nums[3] = 9;
+
+        // for (int i = 0; i < nums.length; i++) {
+        // System.out.println(nums[i]);
+        // }
 
     }
+
 }
