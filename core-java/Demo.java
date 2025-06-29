af7c560ea3fd856204c3670d42b3642d0abf53ca
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-30 01:33:57 +0200

    enhanced for loop

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -17,13 +17,13 @@ public class Demo {
 
         s2.rollNo = 2;
         s2.name = "Beyza";
-        s2.marks = 68;
+        s2.marks = 100;
 
         Student s3 = new Student();
 
         s3.rollNo = 3;
         s3.name = "Endo";
-        s3.marks = 97;
+        s3.marks = 100;
 
         Student students[] = new Student[3];
 
@@ -31,11 +31,15 @@ public class Demo {
         students[1] = s2;
         students[2] = s3;
 
-        for (int i = 0; i < students.length; i++) {
-            System.out.println(students[i].name);
+        // for (int i = 0; i < students.length; i++) {
+        // System.out.println(students[i].name);
+        // }
+
+        for (Student stud : students) {
+            System.out.println(stud.name + " : " + stud.marks);
         }
 
-        // int nums[] = new int[6];
+        // int nums[] = new int[4];
 
         // nums[0] = 4;
         // nums[1] = 8;
@@ -44,6 +48,10 @@ public class Demo {
 
         // for (int i = 0; i < nums.length; i++) {
         // System.out.println(nums[i]);
+        // }
+
+        // for (int n : nums) {
+        // System.out.println(n);
         // }
 
     }
