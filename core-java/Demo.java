43effae3c5aa9ce885ff0b7d32766f6c23b54f13
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-29 19:38:07 +0200

    multidimensional array

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,15 +1,28 @@
 public class Demo {
     public static void main(String args[]) {
 
-        int nums[] = new int[4];
+        int nums[][] = new int[3][4];
 
-        nums[0] = 1;
-        nums[1] = 2;
-        nums[2] = 3;
-        nums[3] = 4;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                nums[i][j] = (int) (Math.random() * 10);
+            }
 
-        for (int i=0; i <= 3;i++){
-            System.out.println(nums[i]);
         }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int n[] : nums) {
+            for (int m : n) {
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
