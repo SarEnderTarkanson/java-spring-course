18ce5b5d1289aa11fc4e0f6a2f1ae3a6d1311cba
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-29 17:39:01 +0200

    creation of Array

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,26 +1,15 @@
-class Calculator {
-
-    int num = 5;
-
-    public int add(int n1, int n2) {
-
-        // System.out.println(num);
-        return n1 + n2;
-    }
-}
-
 public class Demo {
     public static void main(String args[]) {
 
-        int data = 10;
-        Calculator obj = new Calculator();
-        Calculator obj2 = new Calculator();
+        int nums[] = new int[4];
 
-        obj.num = 10;
-        // int r1 = obj.add(3, 4);
-        // System.out.println(data);
-        // System.out.println(r1);
-        System.out.println(obj2.num);
-        System.out.println(obj.num);
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 4;
+
+        for (int i=0; i <= 3;i++){
+            System.out.println(nums[i]);
+        }
     }
 }
