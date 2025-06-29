997fbd5a96b503f4a69c4896355cbf31ed359451
Alparslan Tarkan <atarkan@hotmail.com>
2025-06-29 19:50:47 +0200

    jagged and 3D arrays

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,20 +1,18 @@
 public class Demo {
     public static void main(String args[]) {
 
-        int nums[][] = new int[3][4];
+        //int nums[][][] = new int[3][4][5];
+        int nums[][] = new int[3][];
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
+        // nums[0] = new int[3];
+        // nums[1] = new int[4];
+        // nums[2] = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
                 nums[i][j] = (int) (Math.random() * 10);
             }
 
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(nums[i][j] + " ");
-            }
-            System.out.println();
         }
 
         for (int n[] : nums) {
