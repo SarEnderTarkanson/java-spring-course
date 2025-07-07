8a26922ef7030f6c3a33ca346d378a8181a53d6c
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-07 07:47:11 +0200

    anonymous object

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,56 +1,20 @@
+class A{
 
-class Human {
-
-    private int age;
-    private String name;
-
-    public Human() {
-        age = 18;
-        name = "John Doe";
+    public A(){
+        System.out.println("object created");
     }
 
-    // public Human(int a, String n) {
-    //     age = a;
-    //     name = n;
-    // }
-
-    public Human(int age, String name) {
-        this.age = age;
-        this.name = name;
+    public void show(){
+        System.out.println("in A show");
     }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
+
 
 public class Demo {
     public static void main(String args[]) {
 
-        Human obj = new Human();
-        obj.setAge(39);
-        obj.setName("Alpy");
-
-        Human obj2 = new Human(38, "Makbule");
-
-        System.out.println(obj.getAge());
-        System.out.println(obj.getName());
-
-        System.out.println(obj2.getAge());
-        System.out.println(obj2.getName());
+        new A().show();
+        
     }
 
 }
