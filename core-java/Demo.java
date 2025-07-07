29092967749c52336c06d4a60fca132bc7fc5f1c
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-07 18:13:50 +0200

    method overriding

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,22 +1,20 @@
+class Calc {
+    public int add(int n1, int n2) {
+        return n1 + n2;
+    }
 
+}
 
+class AdvCalc extends Calc{
+public int add(int n1, int n2){
+    return n1 + n2 + 1;
+}
+}
 
 public class Demo {
     public static void main(String args[]) {
-
-        VeryAdvCalc obj = new VeryAdvCalc();
-        int r1 = obj.add(5,4);
-        int r2 = obj.sub(6,3);
-        int r3 = obj.multi(5,4);
-        int r4 = obj.div(6,3);
-        double r5 = obj.power(4, 4);
-
-        System.out.println(r1);
-        System.out.println(r2);
-        System.out.println(r3);
-        System.out.println(r4);
-        System.out.println(r5);
-        
+        AdvCalc obj = new AdvCalc();
+        System.out.println(obj.add(5,6));;
     }
 
 }
