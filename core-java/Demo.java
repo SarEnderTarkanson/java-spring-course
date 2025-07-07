d7db989be67a3fe51a9e444e0b01eecc334ea61c
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-07 08:32:26 +0200

    what is inheritance

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,19 +1,19 @@
-class A{
 
-    public A(){
-        System.out.println("object created");
-    }
-
-    public void show(){
-        System.out.println("in A show");
-    }
-}
 
 
 public class Demo {
     public static void main(String args[]) {
 
-        new A().show();
+        AdvCalc obj = new AdvCalc();
+        int r1 = obj.add(5,4);
+        int r2 = obj.sub(6,3);
+        int r3 = obj.multi(5,4);
+        int r4 = obj.div(6,3);
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
         
     }
 
--- core-java/Hello.java
+++
@@ -1,6 +0,0 @@
-class Hello {
-    public static void main(String args[]) {
-        char num1 = 'c';
-        System.out.println(num1);
-    }
-}
