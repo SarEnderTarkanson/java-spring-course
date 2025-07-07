07601a9b15cd898f54085b6c1b6804574c3bce66
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-07 16:31:08 +0200

    single and multi level inheritance

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -4,16 +4,18 @@
 public class Demo {
     public static void main(String args[]) {
 
-        AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
         int r1 = obj.add(5,4);
         int r2 = obj.sub(6,3);
         int r3 = obj.multi(5,4);
         int r4 = obj.div(6,3);
+        double r5 = obj.power(4, 4);
 
         System.out.println(r1);
         System.out.println(r2);
         System.out.println(r3);
         System.out.println(r4);
+        System.out.println(r5);
         
     }
 
