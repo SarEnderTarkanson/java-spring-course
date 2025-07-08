f29ac995fa478024953a2623725fdf1d6b1c78fe
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-08 23:39:32 +0200

    packages

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,20 +1,11 @@
-class Calc {
-    public int add(int n1, int n2) {
-        return n1 + n2;
-    }
+import tools.other.*;;
 
-}
-
-class AdvCalc extends Calc{
-public int add(int n1, int n2){
-    return n1 + n2 + 1;
-}
-}
 
 public class Demo {
     public static void main(String args[]) {
-        AdvCalc obj = new AdvCalc();
-        System.out.println(obj.add(5,6));;
+
+        Calc obj = new Calc();
+        AdvCalc obj2 = new AdvCalc();
     }
 
 }
