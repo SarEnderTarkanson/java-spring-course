38083048e96e5f1b1da6588c3e24a9260035b2b2
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-11 08:27:56 +0200

    upcasting and downcasting

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,16 +1,24 @@
-class Laptop {
-    String model;
-    int price;
+class A {
+    public void show1() {
+        System.out.println("in A show");
+    }
+}
+
+class B extends A {
+    public void show2() {
+        System.out.println("in B show");
+    }
 }
 
 public class Demo {
     public static void main(String args[]) {
 
-        Laptop obj = new Laptop();
-        obj.model = "Lenovo Yoga";
-        obj.price = 5000;
+        A obj = new B();
 
-        System.out.println(obj);
+        obj.show1();
+
+        B obj2 = (B) obj;
+        obj2.show2();
     }
 
 }
