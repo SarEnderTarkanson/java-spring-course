7656cdf044605dad4e01b8b0b381663883e77f9f
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-15 17:59:08 +0200

    anonymous inner class

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,29 +1,18 @@
 class A {
-
-    int age;
-
     public void show() {
-        System.out.println("in show");
-    }
-
-    static class B {
-        public void config() {
-
-            System.out.println("in config");
-        }
+        System.out.println("in A show");
     }
 }
 
 public class Demo {
     public static void main(String args[]) {
 
-        A obj = new A();
-
+        A obj = new A() {
+            public void show() {
+                System.out.println("in new show");
+            }
+        };
         obj.show();
-
-        A.B obj2 = new A.B();
-
-        obj2.config();
 
     }
 
