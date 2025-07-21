ccd93163ab0f42430e1f5f3821b0abaf4ad28d21
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-21 07:43:51 +0200

    annotations

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,36 +1,22 @@
-enum Laptop {
-    MacBook(2000), XPS(2200), Surface, ThinkPad(1800);
-
-    private int price;
-
-    private Laptop(int price) {
-        this.price = price;
+class A {
+    public void showTheDataWhichBelongsToThisClass() {
+        System.out.println("in A show");
     }
+}
 
-    private Laptop() {
-        price = 500;
+class B extends A {
+    @Override
+    public void showTheDataWhichBelongsToThisClass() {
+        System.out.println("in B show");
     }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
 }
 
 public class Demo {
     public static void main(String args[]) {
 
-        // Laptop lap = Laptop.MacBook;
+        B obj = new B();
 
-        // System.out.println(lap + ": " + lap.getPrice());
-
-        for (Laptop lap : Laptop.values()) {
-            System.out.println(lap + ": " + lap.getPrice());
-        }
+        obj.showTheDataWhichBelongsToThisClass();
 
     }
 
