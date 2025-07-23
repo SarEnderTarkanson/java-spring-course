ae1866867e539d5cbfa83accd28d28c2cecd7a55
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-23 08:14:06 +0200

    ducking exception using throws

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,31 +1,22 @@
-class AlpyException extends Exception {
-    public AlpyException(String string) {
-        super(string);
+class A {
+    public void show() throws ClassNotFoundException {
+
+        Class.forName("Calc");
+
     }
 }
 
 public class Demo {
     public static void main(String args[]) {
-        int i = 20;
-        int j = 0;
 
+        A obj = new A();
         try {
-            j = 18 / i;
-            if (j == 0) {
-                throw new AlpyException("I don't want to print 0.");
-            }
+            obj.show();
+        } catch (ClassNotFoundException e) {
+            
+            e.printStackTrace();
         }
 
-        catch (AlpyException e) {
-            j = 18 / 1;
-            System.out.println("That's the default output." + e);
-        }
-
-        catch (Exception e) {
-            System.out.println("Something went wrong." + e);
-        }
-
-        System.out.println(j);
     }
 
 }
