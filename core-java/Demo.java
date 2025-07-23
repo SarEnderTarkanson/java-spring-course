abb090ab0ff165ba095a26a31dd854361629bf06
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-23 19:09:30 +0200

    user input using BufferedReader and Scanner

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,22 +1,22 @@
-class A {
-    public void show() throws ClassNotFoundException {
-
-        Class.forName("Calc");
-
-    }
-}
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String args[]) {
 
-        A obj = new A();
-        try {
-            obj.show();
-        } catch (ClassNotFoundException e) {
-            
-            e.printStackTrace();
-        }
+        System.out.println("Enter a number");
 
+        // InputStreamReader in = new InputStreamReader(System.in);
+        // BufferedReader bf = new BufferedReader(in);
+
+        Scanner sc = new Scanner(System.in);
+
+        // int num = Integer.parseInt(bf.readLine());
+
+        int num = sc.nextInt();
+        
+        System.out.println(num);
+
+        // bf.close();
     }
 
 }
