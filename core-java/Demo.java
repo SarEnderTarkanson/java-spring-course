cc3ed25bbeecd39fa75991f67f748e9240133bc3
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-23 07:40:49 +0200

    exception throw keyword

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,22 +1,24 @@
 public class Demo {
     public static void main(String args[]) {
-        int i = 2;
+        int i = 0;
         int j = 0;
 
-        int nums[] = new int[5];
+     
 
         try {
             j = 18 / i;
-            System.out.println(nums[1]);
-            System.out.println(nums[5]);
+            if(j == 0){
+                throw new ArithmeticException("I don't want to print 0.");
+            }
         }
 
         catch (ArithmeticException e) {
-            System.out.println("Cannot divide by zero!");
+            j = 18 /1;
+            System.out.println("That's the default output." + e);
         }
 
-        catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Stay in your limit");
+        catch (Exception e){
+            System.out.println("Something went wrong." + e);
         }
 
         System.out.println(j);
