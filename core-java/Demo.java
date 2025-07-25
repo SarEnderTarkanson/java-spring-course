3e562e94c77e9191e816e5c6bbaeed726e69332d
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-25 19:05:41 +0200

    try with resources

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,22 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Demo {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
 
-        System.out.println("Enter a number");
+        int num = 0;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
 
-        // InputStreamReader in = new InputStreamReader(System.in);
-        // BufferedReader bf = new BufferedReader(in);
-
-        Scanner sc = new Scanner(System.in);
-
-        // int num = Integer.parseInt(bf.readLine());
-
-        int num = sc.nextInt();
-        
-        System.out.println(num);
-
-        // bf.close();
     }
 
 }
