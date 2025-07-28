ac3a9c0b997843cd05f316accc67e3a95a62b731
Alparslan Tarkan <atarkan@hotmail.com>
2025-07-28 18:13:01 +0200

    thread priority and sleep

--- core-java/Demo.java
+++ core-java/Demo.java
@@ -1,15 +1,25 @@
-class A extends Thread{
+class A extends Thread {
     public void run() {
         for (int i = 1; i <= 100; i++) {
             System.out.println("hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 
-class B extends Thread{
+class B extends Thread {
     public void run() {
         for (int i = 1; i <= 100; i++) {
             System.out.println("hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
